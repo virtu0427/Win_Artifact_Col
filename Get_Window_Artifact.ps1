c1cecccb1ce2f70ca7035f9988991f78cffdb7c1
dayto <virtu0427@gmail.com>
2025-08-21 22:50:30 +0900

    update net col

--- Get_Window_Artifact.ps1
+++ Get_Window_Artifact.ps1
@@ -1,5 +1,32 @@
 $scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$artifactPath = Join-Path $scriptDirectory "Registry_Artifacts"
+$artifactPath = Join-Path $scriptDirectory "All_Artifacts"
+
+
+$netInfoPath = Join-Path $artifactPath "NetInfo"
+if (!(Test-Path $netInfoPath)) {
+    New-Item -Path $netInfoPath -ItemType Directory | Out-Null
+}
+
+$netCommands = @{
+    "ipconfig.txt"    = "ipconfig /all"
+    "arp.txt"         = "arp -a"
+    "route.txt"       = "route print"
+    "netstat.txt"     = "netstat -ano"
+    "hostname.txt"    = "hostname"
+    "nslookup.txt"    = "nslookup localhost"
+}
+
+foreach ($file in $netCommands.Keys) {
+    $cmd = $netCommands[$file]
+    $outputFile = Join-Path $netInfoPath $file
+    try {
+        Invoke-Expression $cmd | Out-File -FilePath $outputFile -Encoding UTF8
+    } catch {
+        "Error running command: $cmd" | Out-File -FilePath $outputFile -Encoding UTF8
+    }
+}
+
+
 
 <#
 $registryHives = @(
@@ -9,7 +36,7 @@ $registryHives = @(
     "HKCU"
 )
 
-$artifactPath = Join-Path $scriptDirectory "Registry_Artifacts"
+$artifactPath = Join-Path $scriptDirectory "All_Artifacts"
 if (!(Test-Path $artifactPath)) {
     New-Item -Path $artifactPath -ItemType Directory | Out-Null
 }
